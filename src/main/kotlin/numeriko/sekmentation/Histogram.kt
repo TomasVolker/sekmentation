41909ee849c822ea981a3255ab593ecc284d4982
Agustin-Picard <agustin.picard@gmail.com>
2019-02-13 16:03:03 +0100

    Histogram with equilizer and median filter done, not yet tested

---
+++ src/main/kotlin/numeriko/sekmentation/Histogram.kt
@@ -0,0 +1,39 @@
+package numeriko.sekmentation
+
+import tomasvolker.numeriko.core.functions.cumulativeSum
+import tomasvolker.numeriko.core.functions.differences
+import tomasvolker.numeriko.core.interfaces.array1d.double.DoubleArray1D
+import tomasvolker.numeriko.core.interfaces.array1d.double.elementWise
+import tomasvolker.numeriko.core.interfaces.array1d.integer.IntArray1D
+import tomasvolker.numeriko.core.interfaces.array2d.double.DoubleArray2D
+import tomasvolker.numeriko.core.interfaces.factory.doubleArray1D
+import tomasvolker.numeriko.core.interfaces.factory.doubleArray2D
+import tomasvolker.numeriko.core.interfaces.factory.intArray1D
+import tomasvolker.numeriko.core.interfaces.factory.toDoubleArray1D
+import tomasvolker.numeriko.core.operations.reduction.reduce
+import kotlin.math.max
+
+fun IntArray1D.cumulativeSum() = map { reduce { acc, i -> acc + it } }.also { println(it) }.toDoubleArray1D()
+
+interface Histogram<T> {
+    val nBins: Int
+    fun estimateHistogram(input: T): IntArray1D
+}
+
+class ImageHistogram(override val nBins: Int): Histogram<DoubleArray2D> {
+
+    override fun estimateHistogram(image: DoubleArray2D): IntArray1D =
+            intArray1D(nBins) { i ->
+                image.count { it.toInt() == i }
+            }
+
+    fun floatTo8Bits(image: DoubleArray2D) =
+        doubleArray2D(image.shape0, image.shape1) { i0, i1 -> (image[i0,i1] * 256.0).toInt() }
+
+    fun equilizedHistogram(image: DoubleArray2D): DoubleArray1D =
+            estimateHistogram(image).let {
+                it.cumulativeSum()
+                    .elementWise { it * (image.max()!! - image.min()!!) + image.min()!! } // nasty !! action going on
+                    .differences()
+            }
+}
