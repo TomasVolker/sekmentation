7e0205fd007eb427ed58f4b2fe1c83ba96c66637
Agustin-Picard <agustin.picard@gmail.com>
2019-02-13 16:03:03 +0100

    Histogram with equilizer and median filter done, not yet tested

---
+++ src/main/kotlin/numeriko/sekmentation/MedianFilter.kt
@@ -0,0 +1,60 @@
+package numeriko.sekmentation
+
+import tomasvolker.numeriko.core.index.All
+import tomasvolker.numeriko.core.index.Last
+import tomasvolker.numeriko.core.interfaces.array1d.double.DoubleArray1D
+import tomasvolker.numeriko.core.interfaces.array1d.generic.lastIndex
+import tomasvolker.numeriko.core.interfaces.array1d.integer.IntArray1D
+import tomasvolker.numeriko.core.interfaces.array2d.double.DoubleArray2D
+import tomasvolker.numeriko.core.interfaces.array2d.double.MutableDoubleArray2D
+import tomasvolker.numeriko.core.interfaces.array2d.double.applyElementWise
+import tomasvolker.numeriko.core.interfaces.arraynd.double.DoubleArrayND
+import tomasvolker.numeriko.core.interfaces.factory.doubleArray2D
+import tomasvolker.numeriko.core.interfaces.factory.doubleZeros
+import tomasvolker.numeriko.core.interfaces.factory.toDoubleArray1D
+
+interface Filter2D {
+    val kernel: FilterKernel<DoubleArray2D>
+    fun filter(input: DoubleArray2D): DoubleArray2D
+}
+
+interface PipelineFilter2D: Filter2D {
+    override fun filter(input: DoubleArray2D): DoubleArray2D {
+        val destination = doubleZeros(input.shape).as2D().asMutable()
+        filter(input, destination)
+        return destination
+    }
+    fun filter(input: DoubleArray2D, destination: MutableDoubleArray2D): Unit
+}
+
+data class FilterKernel<T: DoubleArrayND>(val window: T, val shape: IntArray1D)
+
+private fun DoubleArray2D.flatten() = List(shape0) { i -> this[i,All] }.flatten().toDoubleArray1D()
+
+private fun DoubleArray1D.median() = sorted()[lastIndex / 2]
+
+class MedianFilter2D(override val kernel: FilterKernel<DoubleArray2D>): PipelineFilter2D {
+
+    override fun filter(input: DoubleArray2D, destination: MutableDoubleArray2D): Unit {
+        val edgeX = input.shape0 - kernel.shape[0]
+        val edgeY = input.shape1 - kernel.shape[1]
+
+        for (i in 0 until input.shape0) {
+            for (j in 0 until input.shape1) {
+                if (i < kernel.shape[0] || i > edgeX || j < kernel.shape[1] || j > edgeY)
+                    destination[i,j] = input[i,j]
+                else
+                    destination[i,j] = convolveWithKernel(input, PixelPosition(i, j)).estimateMedian()
+            }
+        }
+    }
+
+    fun convolveWithKernel(image: DoubleArray2D, position: PixelPosition) =
+            doubleArray2D(kernel.shape[0], kernel.shape[1]) { i0, i1 ->
+                image[i0 + position.x,i1 + position.y] * kernel.window[i0,i1]
+            }
+
+    private fun DoubleArray2D.estimateMedian() = flatten().median()
+
+    inner class PixelPosition(val x: Int, val y: Int)
+}
