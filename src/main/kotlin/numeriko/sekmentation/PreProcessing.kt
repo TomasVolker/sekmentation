fbd3fc3fe20751594bb6f2e6d13b615ab346d966
Agustin-Picard <agustin.picard@gmail.com>
2019-02-19 12:29:46 +0100

    Started the histogram for generic bins

--- src/main/kotlin/numeriko/sekmentation/PreProcessing.kt
+++ src/main/kotlin/numeriko/sekmentation/PreProcessing.kt
@@ -1,14 +1,18 @@
 package numeriko.sekmentation
 
+import tomasvolker.kyplot.dsl.*
 import tomasvolker.numeriko.core.index.All
 import tomasvolker.numeriko.core.interfaces.array1d.double.DoubleArray1D
 import tomasvolker.numeriko.core.interfaces.array1d.generic.lastIndex
 import tomasvolker.numeriko.core.interfaces.array1d.integer.IntArray1D
 import tomasvolker.numeriko.core.interfaces.array2d.double.DoubleArray2D
 import tomasvolker.numeriko.core.interfaces.array2d.double.MutableDoubleArray2D
+import tomasvolker.numeriko.core.interfaces.array2d.generic.toListOfLists
 import tomasvolker.numeriko.core.interfaces.arraynd.double.DoubleArrayND
 import tomasvolker.numeriko.core.interfaces.factory.doubleArray2D
 import tomasvolker.numeriko.core.interfaces.factory.toDoubleArray1D
+import tomasvolker.numeriko.core.linearalgebra.linearSpace
+import kotlin.math.absoluteValue
 
 data class FilterKernel<T: DoubleArrayND>(val window: T, val shape: IntArray1D)
 
@@ -30,6 +34,35 @@ class MedianFilter2D(val kernel: FilterKernel<DoubleArray2D>): PipelineFilter2D 
                     destination[i,j] = convolveWithKernel(input, PixelPosition(i, j)).estimateMedian()
             }
         }
+
+        showFigure {
+            allPlots {
+                position {
+                    columnCount = 2
+                    rowCount = 1
+                }
+            }
+
+            plot {
+                image {
+                    data = input.toListOfLists()
+                }
+                position {
+                    row = 0
+                    column = 0
+                }
+            }
+
+            plot {
+                image {
+                    data = destination.toListOfLists()
+                }
+                position {
+                    row = 0
+                    column = 1
+                }
+            }
+        }
     }
 
     private fun convolveWithKernel(image: DoubleArray2D, position: PixelPosition) =
@@ -42,9 +75,14 @@ class MedianFilter2D(val kernel: FilterKernel<DoubleArray2D>): PipelineFilter2D 
     inner class PixelPosition(val x: Int, val y: Int)
 }
 
-class HistogramEquilizationFilter(override val nBins: Int): PipelineFilter2D, ImageHistogram(nBins) {
+class HistogramEqualizationFilter(override val nBins: Int): PipelineFilter2D, ImageHistogram(nBins) {
     override fun filter(input: DoubleArray2D, destination: MutableDoubleArray2D) {
-        val grayScale = equalizedHistogram(input)
+        val grayScale = equalizedHistogram(floatTo8Bits(input))
+
+        showStem {
+            x = linearSpace(0.0, grayScale.size.toDouble(), grayScale.size)
+            y = grayScale
+        }
 
         for (i in 0 until input.shape0) {
             for (j in 0 until input.shape1) {
--- src/main/kotlin/numeriko/sekmentation/fuzzyregiongrowing/FuzzyRegionGrowing.kt
+++ src/main/kotlin/numeriko/sekmentation/fuzzyregiongrowing/FuzzyRegionGrowing.kt
@@ -5,19 +5,21 @@ import tomasvolker.kyplot.dsl.showImage
 import tomasvolker.kyscript.KyScriptConfig
 import tomasvolker.numeriko.core.dsl.D
 import tomasvolker.numeriko.core.dsl.I
-import tomasvolker.numeriko.core.interfaces.array1d.double.MutableDoubleArray1D
-import tomasvolker.numeriko.core.interfaces.array1d.double.applyElementWise
-import tomasvolker.numeriko.core.interfaces.array1d.integer.IntArray1D
+import tomasvolker.numeriko.core.functions.normalized
 import tomasvolker.numeriko.core.interfaces.array2d.double.DoubleArray2D
 import tomasvolker.numeriko.core.interfaces.array2d.double.MutableDoubleArray2D
+import tomasvolker.numeriko.core.interfaces.array2d.double.elementWise
 import tomasvolker.numeriko.core.interfaces.array2d.generic.toListOfLists
-import tomasvolker.numeriko.core.interfaces.factory.doubleArray1D
 import tomasvolker.numeriko.core.interfaces.factory.doubleArray2D
 import tomasvolker.numeriko.core.interfaces.factory.doubleZeros
-import java.awt.image.BufferedImage
+import tomasvolker.numeriko.core.primitives.squared
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.math.pow
 
+
+fun DoubleArray2D.normalized() =
+        sum().let { sum -> this.elementWise { it / sum } }
 
 class FuzzyRegionGrowing(val kernel: FilterKernel<DoubleArray2D>,
                          val nBins: Int = 256,
@@ -27,28 +29,33 @@ class FuzzyRegionGrowing(val kernel: FilterKernel<DoubleArray2D>,
     override fun filter(input: DoubleArray2D, destination: MutableDoubleArray2D) {
         val auxMatrix: MutableDoubleArray2D = destination.copy()
 
-        HistogramEquilizationFilter(nBins).filter(input, auxMatrix)
-        MedianFilter2D(kernel).filter(auxMatrix, destination)
-        FuzzyConnectedness(pixelSeed, neighborhoodSize).filter(destination, auxMatrix)
-        ThresholdFilter(nBins).filter(auxMatrix, destination)
+        HistogramEqualizationFilter(nBins).filter(input, auxMatrix).also { println("Histogram eq done") }
+        showImage { data = auxMatrix.toListOfLists() }
+        MedianFilter2D(kernel).filter(auxMatrix, destination).also { println("Median filter done") }
+        showImage { data = destination.toListOfLists() }
+        println("max ${destination.max()} min ${destination.min()}")
+        FuzzyConnectedness(pixelSeed, neighborhoodSize).filter(destination, auxMatrix).also { println("Fuzzy done") }
+        showImage { data = auxMatrix.toListOfLists() }
+        ThresholdFilter(nBins).filter(auxMatrix, destination).also { println("Threshold done") }
     }
 }
 
 
-fun loadImage(filename: String): DoubleArray2D {
-    val image = ImageIO.read(File(filename))
-
-    return doubleArray2D(image.height, image.width) { i1, i0 -> image.getRGB(i0, i1) }
-}
+fun loadImage(filename: String): DoubleArray2D =
+        ImageIO.read(File(filename)).let { image ->
+            doubleArray2D(image.height, image.width) { i1, i0 ->
+                image.getRGB(i0, i1) / ((-256.0).pow(3))
+            }
+        }
 
 
 fun main() {
     KyScriptConfig.defaultPythonPath = "python"
 
-    val testImage = loadImage("data/P1_Image_originale.png").also { showImage { data = it.toListOfLists() } }
-    val pixelSeed = PixelCoordinates(testImage.shape0 / 2, testImage.shape1 / 3)
+    val testImage = loadImage("data/P1_Image_originale.png")
+    val pixelSeed = PixelCoordinates(testImage.shape0 - 362, testImage.shape1 - 240)
     val filteredImage: MutableDoubleArray2D = doubleZeros(testImage.shape0, testImage.shape1).asMutable()
-    val medianKernel = FilterKernel(D[D[1,1,1],D[1,1,1],D[1,1,1]], I[3,3])
+    val medianKernel = FilterKernel(D[D[1,1,1],D[1,2,1],D[1,1,1]], I[3,3])
 
     FuzzyRegionGrowing(medianKernel, pixelSeed = pixelSeed).filter(testImage, filteredImage)
 
