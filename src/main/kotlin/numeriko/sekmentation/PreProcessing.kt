675956d47f82b49f1a3581e6ab0a91190bf2c71e
Agustin-Picard <agustin.picard@gmail.com>
2019-02-19 16:30:44 +0100

    Histogram eq fixed

--- src/main/kotlin/numeriko/sekmentation/PreProcessing.kt
+++ src/main/kotlin/numeriko/sekmentation/PreProcessing.kt
@@ -1,6 +1,5 @@
 package numeriko.sekmentation
 
-import tomasvolker.kyplot.dsl.*
 import tomasvolker.numeriko.core.functions.cumulativeSum
 import tomasvolker.numeriko.core.index.All
 import tomasvolker.numeriko.core.interfaces.array1d.double.DoubleArray1D
@@ -8,12 +7,10 @@ import tomasvolker.numeriko.core.interfaces.array1d.generic.lastIndex
 import tomasvolker.numeriko.core.interfaces.array1d.integer.IntArray1D
 import tomasvolker.numeriko.core.interfaces.array2d.double.DoubleArray2D
 import tomasvolker.numeriko.core.interfaces.array2d.double.MutableDoubleArray2D
-import tomasvolker.numeriko.core.interfaces.array2d.generic.toListOfLists
+import tomasvolker.numeriko.core.interfaces.array2d.generic.forEachIndex
 import tomasvolker.numeriko.core.interfaces.arraynd.double.DoubleArrayND
 import tomasvolker.numeriko.core.interfaces.factory.doubleArray2D
 import tomasvolker.numeriko.core.interfaces.factory.toDoubleArray1D
-import tomasvolker.numeriko.core.linearalgebra.linearSpace
-import kotlin.math.absoluteValue
 
 data class FilterKernel<T: DoubleArrayND>(val window: T, val shape: IntArray1D)
 
@@ -45,16 +42,19 @@ class MedianFilter2D(val kernel: FilterKernel<DoubleArray2D>): PipelineFilter2D 
     private fun DoubleArray2D.estimateMedian() = flatten().median()
 }
 
-class HistogramEqualizationFilter: PipelineFilter2D {
+class HistogramEqualizationFilter(histogram: Histogram<DoubleArray2D>): PipelineFilter2D, Histogram<DoubleArray2D> by histogram {
     override fun filter(input: DoubleArray2D, destination: MutableDoubleArray2D) {
-        val grayScale = List(input.size) { i ->
-            input[i,All] to input.flatten().cumulativeSum()[i until i + input.shape0]
+        val list = mutableListOf<ImageValue>()
+
+        input.forEachIndex { i0, i1 ->
+            list.add(ImageValue(input[i0,i1], i0, i1))
         }
 
-        for (i in 0 until input.shape0) {
-            for (j in 0 until input.shape1) {
-                destination[i,j] = input[i,j] / grayScale[i].second[j]
-            }
+        list.sortBy { it.value }
+        list.forEachIndexed { index, value ->
+            destination[value.x, value.y] = index.toDouble() / (list.size - 1)
         }
     }
+
+    inner class ImageValue(val value: Double, val x: Int, val y: Int)
 }
