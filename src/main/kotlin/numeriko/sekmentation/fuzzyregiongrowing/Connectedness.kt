46f4639da265a5093cb184947fed9bc965203fe6
Agustin-Picard <agustin.picard@gmail.com>
2019-02-22 16:45:15 +0100

    Move point, statistic and related to separate files to make it clearer

--- src/main/kotlin/numeriko/sekmentation/fuzzyregiongrowing/Connectedness.kt
+++ src/main/kotlin/numeriko/sekmentation/fuzzyregiongrowing/Connectedness.kt
@@ -7,56 +7,6 @@ import tomasvolker.numeriko.core.primitives.squared
 import java.util.*
 import kotlin.math.*
 
-data class Statistic(
-    val mean: Double,
-    val deviation: Double
-)
-
-data class Point(
-    val x: Int,
-    val y: Int
-)
-
-fun Point.neighborhood(): List<Point> =
-    (x-1..x+1).flatMap { u ->
-        (y-1..y+1).map { v ->
-            Point(u, v)
-        }
-    }
-
-fun Point.isNeighbor(other: Point) =
-    max((other.x - this.x).absoluteValue, (other.y - this.y).absoluteValue) <= 1
-
-fun DoubleArray2D.isValidPoint(point: Point) = point.x in 0 until shape0 && point.y in 0 until shape1
-
-operator fun DoubleArray2D.get(point: Point) = this[point.x, point.y]
-operator fun MutableDoubleArray2D.set(point: Point, value: Double) { this[point.x, point.y] = value }
-
-fun <T> List<T>.allPairs(): List<Pair<T, T>> =
-    this.mapIndexed { i, value ->
-        (i+1 until size).map { j -> value to this[j] }
-    }.flatten()
-
-fun List<Double>.deviation(): Double {
-    val mean = average()
-    return sqrt(sumByDouble { (it - mean).squared() } / (size - 1))
-}
-
-fun List<Double>.computeStatistics() = Statistic(
-    mean = average(),
-    deviation = deviation()
-)
-
-fun DoubleArray2D.estimateConnectivityStatistic(
-    point: Point,
-    connectivityFunction: (a: Double, b: Double)->Double
-): Statistic = point.neighborhood()
-        .filter { isValidPoint(it) }
-        .allPairs()
-        .filter { it.first.isNeighbor(it.second) }
-        .map { connectivityFunction(this[it.first], this[it.second]) }
-        .computeStatistics()
-
 
 class FuzzyConnectednessAlgorithm(
     val image: DoubleArray2D,
